2fbeb464f2a17414129457555b2293feffca333c
indresh-007 <140510462+indresh-007@users.noreply.github.com>
2026-05-25 21:59:34 +0530

    Day 23 -  JavaScript Promises

--- Day-23/index.js
+++ Day-23/index.js
@@ -93,7 +93,7 @@ let getUser = new Promise(function(resolve, reject) {
         name: 'Indresh Dwivedi',
         email: 'indresh@gmail.com',
         password: 'indresh.password',
-        permissions: [ 'db', 'hr', 'dev']
+        permissions: [ 'db', 'dev']
     };
     resolve(user);
 });
@@ -122,4 +122,172 @@ getUser
 })
 
 // Throw an error from the .then() handler
+
+getUser
+.then(function(user) {
+    console.log(`Got User ${user.name}`);
+    
+    if(!user.permissions.includes("hr")) {
+        throw new Error("You are not allowed to access the HR module");
+    }
+
+    return user.email;
+})
+.then((email) => {
+    console.log(`User email is ${email}`);
+})
+.catch((error) => {
+    console.error(error);
+})
+
+// Rule 3: You can rethrow from the .catch() handler to handle the error later. In this case, the control will go to the next closest .catch() handler.
+
+/*
+let promise401 = new Promise(function(resolve, reject) {
+    reject(401)
+});
+
+promise401
+.catch((error) => {
+    console.log(error);
+    if (error === 401) {
+        console.log("Rethrowing 401");
+        throw error;
+    } else {
+        // Do Something
+    }
+})
+.then((result) => {
+    console.log(result);
+})
+.catch((error) => {
+    console.log(`handling ${error} here`);
+})
+*/
+
+
+// Rule 4 - Unlike .then() and .catch(), the .finally() handler doesn't process the result value or error. It just passes the result as is to the next handler.
+
+// Create a Promise
+/*
+let promiseFinally = new Promise(function(resolve, reject) {
+    resolve('Testing Finally.');
+});
+
+promiseFinally
+.finally(function() {
+    console.log("Running Finally!")
+})
+.then(function(result) {
+    console.log(result);
+})
+*/
+
+
+// Rule 5 - Calling the .then() handler method multiple times on a single promise is NOT chaining.
+/*
+promise
+.then((result) => {
+    // Do Something
+    return 101;
+})
+.then((result) => {
+    // result // 101
+    // throw error
+})
+.catch((error) => {
+
+})
+
+// This is not Chaining Promises
+
+// Create a Promise
+{
+  let promise = new Promise(function (resolve, reject) {
+    resolve(10);
+  });
+
+  // Calling the .then() method multiple times on a single promise - It's not a chain.
+  promise.then(function (value) {
+    value++;
+    return value;
+  })
+  .then(function (value) {
+    value = value + 10;
+    return value;
+  })
+  .then(function (value) {
+    value = value + 20;
+    console.log(value);
+    return value;
+  });
+}
+*/
+
+// Handle Multiple Promises
+
+
+// promise.all([promises])
+
+const BULBASAUR_POKEMONS_URL = 'https://pokeapi.co/api/v2/pokemon/bulbasaur';
+const RATICATE_POKEMONS_URL = 'https://pokeapi.co/api/v2/pokemon/raticate';
+const KAKUNA_POKEMONS_URL = 'https://pokeapi.co/api/v2/pokemon/kakuna';
+
+
+let promise_1 = getPromise(BULBASAUR_POKEMONS_URL);
+let promise_2 = getPromise(RATICATE_POKEMONS_URL);
+let promise_3 = getPromise(KAKUNA_POKEMONS_URL);
+
+/*
+Promise.all([promise_1, promise_2, promise_3])
+.then((result) => {
+    console.log(result);
+})
+.catch((error) => {
+    console.error(error);
+})
+*/
+
+// promise.any([promises])
+
+/*
+Promise.any([promise_1, promise_2, promise_3])
+.then((result) => {
+    console.log(result);
+})
+.catch((error) => {
+    console.error(error);
+})
+*/
+
+// promise.allSettled([promises])
+// settling a promise = fulfilling(resolve) + rejecting
+
+/*
+Promise.allSettled([promise_1, promise_2, promise_3])
+.then((result) => {
+    console.log(result);
+})
+.catch((error) => {
+    console.error(error);
+})
+*/
+
+// promise.race([promises])
+
+/*
+Promise.race([promise_1, promise_2, promise_3])
+.then((result) => {
+    console.log(result);
+})
+.catch((error) => {
+    console.error(error);
+})
+*/
+
+Promise.resolve()
+// let promise = new Promise(resolve => resolve(value));
+
+Promise.reject()
+// let promise = new Promise((resolve, reject) => reject(error));
 
---
+++ Day-23/promise.js
@@ -0,0 +1,10 @@
+function getPromise(URL) {
+    return fetch(URL)
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(`HTTP error ${response.status}`);
+        }
+        return response.json();
+    });
+}
+
